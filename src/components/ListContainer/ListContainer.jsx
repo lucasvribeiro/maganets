dbd04f75a046b2f7a9b993cb2758050f56aad9bd
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-12-31 14:44:10 -0300

    Created ListContainer Component

---
+++ src/components/ListContainer/ListContainer.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const ListContainerStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  grid-gap: 32px 60px;
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 50px;
+  box-sizing: border-box;
+  justify-content: center;
+
+  @media only screen and (max-width: 1200px) {
+    grid-template-columns: repeat(3, auto);
+    grid-gap: 32px 16px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, auto);
+  }
+
+  @media only screen and (max-width: 480px) {
+    grid-template-columns: repeat(1, auto);
+  }
+`;
+
+const ListContainer = ({ children }) => {
+  return <ListContainerStyled>{children}</ListContainerStyled>;
+};
+
+ListContainer.propTypes = {
+  children: PropTypes.any,
+};
+
+ListContainer.deafultProps = {};
+
+export default ListContainer;
