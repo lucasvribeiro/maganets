2956ac6afe7435c6703d3e8d794ef8492aa8ffda
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-12-31 10:01:50 -0300

    Created SearchBox Component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,46 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const StyledSearchBox = styled.div``;
+const StyledSearchBox = styled.div`
+  i {
+    position: relative;
+    left: 16px;
+    top: -34px;
 
-const SearchBox = ({ children, className }) => {
-  return <StyledSearchBox className={className}>{children}</StyledSearchBox>;
+    color: #999999;
+  }
+
+  input {
+    width: 100%;
+    height: 48px;
+    border-radius: 4px;
+
+    box-sizing: border-box;
+
+    border: 1px solid #cfcfcf;
+    padding: 0 40px;
+
+    &:focus {
+      border: 1px solid #370f53;
+      outline: none;
+    }
+  }
+`;
+
+const SearchBox = ({ children, placeholder, value, disabled, onChange }) => {
+  return (
+    <StyledSearchBox>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+
+      <i className="fas fa-search" />
+    </StyledSearchBox>
+  );
 };
 
 SearchBox.propTypes = {
