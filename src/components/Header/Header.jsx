20d0720b00f480e25d0bb119f7354b0261237be4
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-12-31 10:02:00 -0300

    Created Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const StyledHeader = styled.div``;
+const StyledHeader = styled.div`
+  height: fit-content;
+  min-height: 25vh;
+  width: 100vw;
+  background: #5a2d82;
+  border-top: 10px solid #370f53;
+  color: #ffffff;
+  padding: 30px 50px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+`;
 
 const Header = ({ children, className }) => {
   return <StyledHeader className={className}>{children}</StyledHeader>;
