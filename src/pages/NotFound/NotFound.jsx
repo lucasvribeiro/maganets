6254c600b1ae7c149c13c3e75b2810a03ad333aa
lucasvribeiro <lucasvribeiro14@gmail.com>
2022-01-02 14:44:38 -0300

    Created NotFound Page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./NotFound.css";
+
 const NotFound = () => {
-  return <div>not found page</div>;
+  return (
+    <div className="not-found">
+      <i className="fas fa-exclamation-triangle" />
+      <h1>Página não encontrada.</h1>
+    </div>
+  );
 };
 
 export default NotFound;
