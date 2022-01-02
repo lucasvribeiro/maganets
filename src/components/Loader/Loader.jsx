57bf967752310b16223c1862ad652685d595e625
lucasvribeiro <lucasvribeiro14@gmail.com>
2022-01-02 14:35:30 -0300

    Created Loader Component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Spin } from "antd";
+
+import "antd/lib/spin/style/index.css";
+
+const StyledLoader = styled(Spin)`
+  max-height: none !important;
+  height: 100vh !important;
+
+  .ant-spin-dot-item {
+    background: #5a2d82 !important;
+  }
+`;
+
+const Loader = ({ children, size, loading }) => {
+  return (
+    <StyledLoader size={size} spinning={loading}>
+      {children}
+    </StyledLoader>
+  );
+};
+
+Loader.propTypes = {
+  size: PropTypes.string,
+  loading: PropTypes.bool,
+};
+
+Loader.defaultProps = {
+  size: "middle", // "small", "middle" or "large"
+  loading: false,
+};
+
+export default Loader;
